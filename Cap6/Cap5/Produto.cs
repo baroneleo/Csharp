6bfa1279beea44961278011054c8fb4d3fd6b91d
Leonardo Barone <leonardo.barone1996@gmail.com>
2023-06-06 19:36:16 -0300

    Add new assembly

---
+++ Cap6/Cap5/Produto.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap5
+{
+    internal class Produto
+    {
+        #region Exercício Produto cap. 4
+
+        /*
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Quantidade * Preco;
+        }
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+        */
+
+        #endregion
+
+        #region Exercício remodelado com construtores
+
+        /*
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Quantidade * Preco;
+        }
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+        */
+
+        #endregion
+
+        #region Exercício remodelado com Sobrecarga
+
+        /*
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto()
+        {
+
+        }
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        public Produto(string nome, double preco) 
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = 5;
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return Quantidade * Preco;
+        }
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+        */
+
+        #endregion
+
+        #region Exercício remodelado com Sobrecarga "Sintaxe alternativa"
+
+        /*
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        public Produto() 
+        { 
+        
+        }
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return Quantidade * Preco;
+        }
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+        */
+
+        #endregion
+
+        #region Exercicio remodelado referenciando um um construtor com outro construtor
+
+        /*
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        public Produto()
+        {
+            Quantidade = 10;
+        }
+        public Produto(string nome, double preco) : this()
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+        public Produto(string nome, double preco, int Quantidade) : this(nome, preco) 
+        {
+            Quantidade = Quantidade;
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return Quantidade * Preco;
+        }
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+        */
+
+        #endregion
+
+        #region Exercício utilizando encapsulamento
+
+        /*
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+        public Produto()
+        {
+
+        }        
+        public Produto(string nome, double preco, int quantidade)
+        {   
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+        public string GetNome() 
+        {
+            return _nome;
+        }
+        public void SetNome(string nome) 
+        {
+            if( nome != null && nome.Length > 1 ) 
+            {
+                _nome = nome;
+            }
+        }
+        public double GetPreco()
+        {
+            return _preco;
+        }
+        public int GetQuantidade()
+        {
+            return _quantidade;
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return _quantidade * _preco;
+        }
+        public void AdicionarProduto(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+        public void RemoverProduto(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + _preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + _quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+        */
+
+        #endregion
+
+        #region Utilização de Properties
+
+        /*
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+        public Produto()
+        {
+
+        }
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+        public string Nome
+        {
+            get { return _nome; }
+            set {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;                
+                }
+            }
+        }
+        public double Preco
+        {
+            get { return _preco; }
+        }
+        public int Quantidade
+        {
+            get { return _quantidade; }
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return _quantidade * _preco;
+        }
+        public void AdicionarProduto(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+        public void RemoverProduto(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + _preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + _quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+        */
+
+        #endregion
+
+        #region Utilização de autoproperties
+
+        /*
+        private string _nome;
+        public double Preco { get; private set; }
+        public int Quantidade {get; private set; }
+        
+        public Produto()
+        {
+
+        }        
+        
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }        
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Quantidade * Preco;
+        }
+        
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+        */
+
+        #endregion
+
+    }
+}
---
+++ Cap6/Cap5/Program.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Globalization;
+
+using Cap5;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Exercício Produto cap. 4
+
+            /*
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qtd = int.Parse(Console.ReadLine());
+            p.AdicionarProduto(qtd);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser removido ao estoque: ");
+            qtd = int.Parse(Console.ReadLine());
+            p.RemoverProduto(qtd);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            */
+
+            #endregion
+
+            #region Exercício remodelado com construtores
+
+            /*
+            Console.WriteLine("Entre os dados do produto:");
+            
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Produto p = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qtd = int.Parse(Console.ReadLine());
+            p.AdicionarProduto(qtd);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser removido ao estoque: ");
+            qtd = int.Parse(Console.ReadLine());
+            p.RemoverProduto(qtd);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            */
+
+            #endregion
+
+            #region Exercício remodelado com Sobrecarga
+
+            /*
+            Console.WriteLine("Entre os dados do produto:");
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Produto p = new Produto(nome, preco);
+
+            Produto p2 = new Produto();
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qtd = int.Parse(Console.ReadLine());
+            p.AdicionarProduto(qtd);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser removido ao estoque: ");
+            qtd = int.Parse(Console.ReadLine());
+            p.RemoverProduto(qtd);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            */
+
+            #endregion
+
+            #region Exercício remodelado com Sobrecarga "Sintaxe alternativa"
+
+            /*
+            Console.WriteLine("Entre os dados do produto:");
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Produto p = new Produto(nome, preco);
+
+            Produto p2 = new Produto();
+
+            Produto p3 = new Produto
+            {
+                Nome = "TV",
+                Preco = 900.00, 
+                Quantidade = 20
+            };
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qtd = int.Parse(Console.ReadLine());
+            p.AdicionarProduto(qtd);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser removido ao estoque: ");
+            qtd = int.Parse(Console.ReadLine());
+            p.RemoverProduto(qtd);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            */
+
+            #endregion
+
+            #region Exercicio remodelado referenciando um um construtor com outro construtor 
+
+            /*
+            Console.WriteLine("Entre os dados do produto:");
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Produto p = new Produto(nome, preco);
+
+            Produto p2 = new Produto();
+
+            Produto p3 = new Produto
+            {
+                Nome = "TV",
+                Preco = 900.00,
+                Quantidade = 20
+            };
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qtd = int.Parse(Console.ReadLine());
+            p.AdicionarProduto(qtd);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser removido ao estoque: ");
+            qtd = int.Parse(Console.ReadLine());
+            p.RemoverProduto(qtd);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            */
+
+            #endregion
+
+            #region Utilização de encapsulamento
+
+            /*
+            Produto p = new Produto("TV", 900.00, 10);
+
+            p.SetNome("T"); // A condição na propriedade não permite a mudança no set
+            Console.WriteLine(p.GetNome());
+
+            p.SetNome("TV 4K"); // Aqui ja permite a mudança no set
+            Console.WriteLine(p.GetNome());
+
+            Console.WriteLine(p.GetPreco());
+            Console.WriteLine(p.GetQuantidade());
+            */
+
+            #endregion
+
+            #region Utilização de Properties
+
+            /*
+            Produto p = new Produto("TV", 900.00, 10);
+
+            p.Nome = "T";
+            
+            Console.WriteLine(p.Nome); // Aqui não permite a mudança no set
+            Console.WriteLine(p.Preco);
+            Console.WriteLine(p.Quantidade);
+            */
+
+            #endregion
+
+            #region Utilização de autoproperties
+
+            /*
+            Produto p = new Produto("TV", 900.00, 10);
+
+            p.Nome = "T";
+
+            Console.WriteLine(p.Nome); // Aqui não permite a mudança no set
+            Console.WriteLine(p.Preco);
+            Console.WriteLine(p.Quantidade);
+            */
+
+            #endregion
+
+            #region Ordem sugerida para estrutura da classe
+
+            /*
+             * ATRIBUTOS PRIVADOS
+             * AUTO PROPERTIES
+             * CONSTRUTORES
+             * PROPRIERTIES CUSTOMIZADAS
+             * OUTROS METODOS DA CLASSE
+            */
+
+            #endregion
+
+            #region Exercício Conta Bancária
+
+            /*
+            ContaBancaria conta;
+
+            Console.Write("Digite o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do titular: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá deposito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+
+            if (resp == 's' || resp == 'S')
+            {
+                Console.Write("Entre com o valor do depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Digite um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados atualizados:");
+            Console.Write(conta);
+
+            Console.Write("Digite um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados atualizados:");
+            Console.WriteLine(conta);
+
+            Console.Write(conta);
+            */
+
+            #endregion
+
+        }
+    }
+}
