963c86e39bd5cc6aad7e410a3af1b6d7221a5080
Leonardo Barone <leonardo.barone1996@gmail.com>
2023-07-03 11:24:16 -0300

    switch case

--- Cap6/Cap7/Program.cs
+++ Cap6/Cap7/Program.cs
@@ -6,7 +6,107 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region VAR
+
+            /*
+            var x = 10;
+            var y = 10.2;
+            var z = "Leo";
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+            int w = z;
+            */
+
+            #endregion
+
+            #region SWITCH CASE
+
+            /*
+            int x = int.Parse(Console.ReadLine());
+
+            string day;
+
+            // IF ELSE
+
+            if (x == 1)
+            {
+                day = "Sunday";
+            }
+            else if (x == 2)
+            {
+                day = "Monday";
+            }
+            else if (x == 3) 
+            {
+                day = "Tuesday";
+            }
+            else if (x == 4) 
+            {
+                day = "Wednesday";
+            }
+            else if(x == 5) 
+            {
+                day = "Thursday";
+            }
+            else if (x == 6)
+            {
+                day = "Friday";
+            }
+            else if (x == 7)
+            {
+                day = "Saturday";
+            }
+            else
+            {
+                day = "Invlaid value";
+            }
+
+            Console.WriteLine("Dia: " + day);
+
+            // SWITCH CASE
+
+            switch (x)
+            { 
+                case 1:
+                    day = "Sunday";
+                    break;        
+                case 2:
+                    day = "Monday";
+                    break;
+                case 3:
+                    day = "Tuesday";
+                    break;            
+                case 4:
+                    day = "Wednesday";
+                    break;            
+                case 5:
+                    day = "Thursday";
+                    break;            
+                case 6:
+                    day = "Friday";
+                    break;            
+                case 7:
+                    day = "Saturday";
+                    break;
+                default:
+                    day = "Invalid value";
+                    break;
+            }
+
+            Console.WriteLine("Day: " + day);
+            */
+
+            #endregion
+
+            #region CONDICIONAL TERNÁRIA
+
+
+
+            #endregion
+
         }
     }
 }
