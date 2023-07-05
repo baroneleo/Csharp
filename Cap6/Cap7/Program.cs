55924c8a481cc228c117f917ecb36d82d9c600ff
Leonardo Barone <leonardo.barone1996@gmail.com>
2023-07-05 14:05:10 -0300

    DateTime

--- Cap6/Cap7/Program.cs
+++ Cap6/Cap7/Program.cs
@@ -187,6 +187,49 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
             #endregion
 
+            #region DATETIME
+
+            // CONSTRUTORES MAIS UTILIZADOS
+
+            DateTime d2 = new DateTime(2018, 11, 25); // Instancia dia, mes e ano passados como parametro
+            Console.WriteLine(d2);
+
+            DateTime d3 = new DateTime(2018, 11, 25, 20, 45, 03); // Instancia horas, minutos, segundos, dia, mes e ano passados como parametro
+            Console.WriteLine(d3);
+
+            // BUILDERS BÁSICOS
+
+            DateTime d1 = DateTime.Now; // Horário atual local
+            Console.WriteLine(d1);
+            Console.WriteLine(d1.Ticks);
+
+            DateTime d4 = DateTime.Today; // Data de hoje mas com o horário 00:00
+            Console.WriteLine(d4);
+
+            DateTime d5 = DateTime.UtcNow; // Horário no meridiano de Greenwich UTC
+            Console.WriteLine(d5);
+
+            // PARSE
+
+            DateTime d6 = DateTime.Parse("2000-08-15"); // Formato utilizado em banco de dados
+            Console.WriteLine(d6);
+            DateTime d7 = DateTime.Parse("2000-08-15 13:05:58");
+            Console.WriteLine(d7);
+
+            DateTime d8 = DateTime.Parse("15/08/2000");
+            Console.WriteLine(d8);
+            DateTime d9 = DateTime.Parse("15/08/2000 13:05:58");
+            Console.WriteLine(d9);
+
+            // PARSEEXACT
+
+            DateTime d10 = DateTime.ParseExact("2000-08-15", "yyyy-MM-dd", CultureInfo.InvariantCulture); // Passa o parametro no formato de banco de dados e como ele tem que ser analisado respectivamente
+            Console.WriteLine(d10);
+            DateTime d11 = DateTime.ParseExact("15/08/2000 13:05:58", "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+            Console.WriteLine(d11);
+
+            #endregion
+
         }
     }
 }
