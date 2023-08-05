26c296eb0f5da94acdd940207773570b05ae9db9
Leonardo Barone <leonardo.barone1996@gmail.com>
2023-08-05 09:48:01 -0300

    DateTimeKind e ISO 8601

--- Cap6/Cap7/Program.cs
+++ Cap6/Cap7/Program.cs
@@ -369,7 +369,112 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
             #region PROPRIEDADES COM TIMESPAN
 
+            /*
+            TimeSpan t1 = TimeSpan.MaxValue;
+            TimeSpan t2 = TimeSpan.MinValue;
+            TimeSpan t3 = TimeSpan.Zero;
 
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            */
+
+            /*
+            TimeSpan t = new TimeSpan(2, 4, 5, 7, 11);
+
+            Console.WriteLine(t);
+
+            Console.WriteLine("1) Date: " + t.Days);
+            Console.WriteLine("5) Hour: " + t.Hours);;
+            Console.WriteLine("7) Milisecond: " + t.Milliseconds);
+            Console.WriteLine("8) Minute: " + t.Minutes);
+            Console.WriteLine("10) Second: " + t.Seconds);
+            Console.WriteLine("11) Ticks: " + t.Ticks);
+
+            Console.WriteLine("1) Date: " + t.TotalDays);
+            Console.WriteLine("5) Hour: " + t.TotalHours); ;
+            Console.WriteLine("8) Minute: " + t.TotalMinutes);
+            Console.WriteLine("10) Second: " + t.TotalSeconds);
+            Console.WriteLine("7) Milisecond: " + t.TotalMilliseconds);
+            */
+
+            // OPERAÇÕES COM TIMESPAN
+
+            /*
+            TimeSpan t1 = new TimeSpan(1, 30, 10);
+            TimeSpan t2 = new TimeSpan(0, 10, 5);
+
+            TimeSpan sum = t1.Add(t2);
+            Console.WriteLine(sum);
+            
+            TimeSpan diff = t1.Subtract(t2);
+            Console.WriteLine(diff);
+
+            TimeSpan mult = t2.Multiply(2.0);
+            Console.WriteLine(mult);
+
+            TimeSpan div = t2.Divide(2.0);
+            Console.WriteLine(div);
+            */
+
+            #endregion
+
+            #region DATETIMEKIND E PADRÃO ISO 8601
+
+            /*
+            DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 38, DateTimeKind.Local);
+            Console.WriteLine("d1: " + d1);
+            Console.WriteLine("d1 Kind: " + d1.Kind);
+            Console.WriteLine("d1 to Local: " + d1.ToLocalTime());
+            Console.WriteLine("d1 to Utc: " + d1.ToUniversalTime());
+
+            Console.WriteLine();
+
+            DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 38, DateTimeKind.Utc);
+            Console.WriteLine("d2: " + d2);
+            Console.WriteLine("d2 Kind: " + d2.Kind);
+            Console.WriteLine("d2 to Local: " + d2.ToLocalTime());
+            Console.WriteLine("d2 to Utc: " + d2.ToUniversalTime());
+
+            Console.WriteLine();
+
+            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 38);
+            Console.WriteLine("d3: " + d3);
+            Console.WriteLine("d3 Kind: " + d3.Kind);
+            Console.WriteLine("d3 to Local: " + d3.ToLocalTime());
+            Console.WriteLine("d3 to Utc: " + d3.ToUniversalTime());
+
+            Console.WriteLine();
+
+            DateTime d4 = DateTime.Now;
+            Console.WriteLine("d4: " + d4);
+            Console.WriteLine("d4 Kind: " + d4.Kind);
+            Console.WriteLine("d4: to Local" + d4.ToLocalTime());
+            Console.WriteLine("d4: to Utc" + d4.ToUniversalTime());
+            */
+
+            // PADRÃO ISO 8601
+
+            /*
+            DateTime d1 = DateTime.Parse("2000-08-15 13:05:58");
+            Console.WriteLine("d1: " + d1);
+            Console.WriteLine("d1 Kind: " + d1.Kind);
+            Console.WriteLine("d1 to Local: " + d1.ToLocalTime());
+            Console.WriteLine("d1 to Utc: " + d1.ToUniversalTime());
+
+            Console.WriteLine();
+
+            DateTime d2 = DateTime.Parse("2000-08-15T13:05:58Z");
+            Console.WriteLine("d2: " + d2);
+            Console.WriteLine("d2 Kind: " + d2.Kind);
+            Console.WriteLine("d2 to Local: " + d2.ToLocalTime());
+            Console.WriteLine("d2 to Utc: " + d2.ToUniversalTime());
+
+            Console.WriteLine();
+
+            Console.WriteLine(d2.ToString("yyy-MM-ddTHH:mm:ssZ")); // Cuidado!
+            Console.WriteLine(d2.ToUniversalTime().ToString("yyy-MM-ddTHH:mm:ssZ"));
+            */
 
             #endregion
 
