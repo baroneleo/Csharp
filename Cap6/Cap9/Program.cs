fe064396847adb8352e8f9676fb733265de58b20
Leonardo Barone <leonardo.barone1996@gmail.com>
2023-08-21 19:40:46 -0300

    New Exercise

--- Cap6/Cap9/Program.cs
+++ Cap6/Cap9/Program.cs
@@ -126,6 +126,62 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
             #endregion
 
+            #region Exercício proposto
+
+            // ENUNCIADO
+
+            /*
+            Ler os dados de um pedido com N itens (N fornecido pelo usuário). Depois, mostrar um
+            sumário do pedido conforme exemplo (próxima página). Nota: o instante do pedido deve ser
+            o instante do sistema: DateTime.Now
+            */
+
+            // RESOLUÇÃO
+
+            /*
+            Console.WriteLine("Enter client data: ");
+            
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter {i} item data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Product product = new Product(productName, price);
+
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                OrderItem orderItem = new OrderItem(quantity, price, product);
+
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+            */
+
+            #endregion
+
         }
     }
 }
