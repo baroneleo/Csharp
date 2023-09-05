4ce4a07ebd832f88b98dcd597499d775175cca95
Leonardo Barone <leonardo.barone1996@gmail.com>
2023-09-05 12:19:47 -0300

    Adding proposed exercise

---
+++ Cap6/Cap10/Exercise4/TaxPayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap10.Exercise4
+{
+    abstract class TaxPayer
+    {
+        public string Name { get; set; }
+
+        public double AnualIncome { get; set; }
+
+        public TaxPayer() { }
+
+        public TaxPayer(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+
+        public abstract double Tax();
+    }
+}
