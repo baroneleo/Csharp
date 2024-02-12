b5585eb31b4998e45c19d6e1219621ff523bff60
Leonardo Barone <leonardo.barone1996@gmail.com>
2024-02-12 11:30:15 -0300

    Interface Exercise

--- Cap6/Cap14/Program.cs
+++ Cap6/Cap14/Program.cs
@@ -9,6 +9,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Enter rental data");
 
             Console.Write("Car model: ");
@@ -34,6 +35,32 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
             Console.WriteLine("INVOICE:");
             Console.WriteLine(carRental.Invoice);
+            */
+
+            // EXERCÍCIO
+
+            /*
+            Console.WriteLine("Enter contract data:");
+            Console.Write("Number: ");
+            int contractNumber = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime contractDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Contract value: ");
+            double contractValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter number of installments: ");
+            int months = int.Parse(Console.ReadLine());
+
+            Contract myContract = new Contract(months, contractDate, contractValue);
+
+            ContractService contractService = new ContractService(new PaypalService());
+            contractService.ProcessContract(myContract, months);
+
+            Console.WriteLine("Installments:");
+            foreach (Installment installment in myContract.Installments)
+            {
+                Console.WriteLine(installment);
+            }
+            */
         }
     }
 }
